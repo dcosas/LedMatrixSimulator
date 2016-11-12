70df9d8a2fe32ec5b443fbb8fdc9448bd4d16a58
Cosas, Deian (Consultant) <Deian.Cosas@brooks.com>
2016-11-13 01:51:53 +0200

    Refactored code.
    Added MatrixDraw class which handles the drawing of the LEDs

--- LedMatrixSimulator/MainWindow.xaml.cs
+++ LedMatrixSimulator/MainWindow.xaml.cs
@@ -1,112 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LedMatrixSimulator
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+{ 
     public partial class MainWindow : Window
     {
-        List<Ellipse> ellipseCollection = new List<Ellipse>();
+        MatrixDraw mDraw = null; 
+
         public MainWindow()
         {
             InitializeComponent();
-            
-        }
-
-        private void DrawMatrixFromConfig()
-        {
-            var _matrixHeight = Convert.ToInt16(matrixHeightTBox.Text.ToString());
-            var _matrixWidth = Convert.ToInt16(matrixWidthTBox.Text.ToString());
-            var _dotSize = Convert.ToInt16(dotSizeTBox.Text.ToString());
-            var _dotSpacing = Convert.ToInt16(dotSpacingTBox.Text.ToString());
-
-            double left = 0.0f;
-            double top = 0.0f;
-
-            myCanvas.Children.RemoveRange(0, myCanvas.Children.Count);
-            
-            //StackPanel panel = new StackPanel();
-            for ( var i = 0; i < _matrixHeight; i++)
-            {
-                for ( var j=0; j<_matrixWidth; j++)
-                {
-                    Ellipse ellipse = new Ellipse();
-                    SolidColorBrush solidBrush = new SolidColorBrush(Color.FromRgb(255, 255, 255));
-                    ellipse.Fill = solidBrush;
-                    ellipse.StrokeThickness = 0;
-                    ellipse.Width = _dotSize;
-                    ellipse.Height = _dotSize;
-                    left += _dotSpacing;                   
-                    ellipse.Margin = new Thickness(left, top, 0, 0);
-                    myCanvas.Children.Add(ellipse);
-
-                    ellipseCollection.Add(ellipse);
-                    //panel.Children.Add(elipse);
-                    //this.Content = panel;
-                }
-                top += _dotSpacing;
-                left = 0;
-            }
-        }
+            mDraw = MatrixDraw.Create();
+        }       
 
         private void drawMatrixBtn_Click(object sender, RoutedEventArgs e)
         {
-            DrawMatrixFromConfig();
+           mDraw.DrawMatrixFromConfig();
         }
 
         private void testBtn_Click(object sender, RoutedEventArgs e)
         {
-            SetLed(Convert.ToInt16(rowTBox.Text.ToString()),
+            mDraw.SetLed(Convert.ToInt16(rowTBox.Text.ToString()),
                    Convert.ToInt16(colTBox.Text.ToString()),
                    Color.FromRgb(Convert.ToByte(rTBox.Text.ToString()),
                                  Convert.ToByte(gTBox.Text.ToString()),
                                  Convert.ToByte(bTBox.Text.ToString())));
-
-        }
-
-        private void RedrawMatrix()
-        {
-            var _matrixHeight = Convert.ToInt16(matrixHeightTBox.Text.ToString());
-            var _matrixWidth = Convert.ToInt16(matrixWidthTBox.Text.ToString());
-
-            myCanvas.Children.RemoveRange(0, myCanvas.Children.Count);
-            
-            for (var i = 0; i < _matrixHeight * _matrixWidth; i++)
-            {
-                myCanvas.Children.Add(ellipseCollection.ElementAt(i));
-            }
-        }
-
-        public void SetLed(short line, short column, Color color)
-        {
-            if (line > Convert.ToInt16(matrixHeightTBox.Text.ToString()))
-            {
-                MessageBox.Show("Line exceeds the maximum height");
-                return;
-            }
-            if (column > Convert.ToInt16(matrixWidthTBox.Text.ToString()))
-            {
-                MessageBox.Show("Column exceeds the maximum width");
-                return;
-            }
-
-            var ellipse = ellipseCollection.ElementAt((line - 1) * Convert.ToInt16(matrixWidthTBox.Text.ToString()) + (column - 1));
-            ellipse.Fill = new SolidColorBrush(color);            
-            RedrawMatrix();
 
         }
     }
--- LedMatrixSimulator/MatrixDraw.cs
+++ LedMatrixSimulator/MatrixDraw.cs
@@ -3,10 +3,102 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace LedMatrixSimulator
 {
     class MatrixDraw
     {
+        List<Ellipse> ellipseCollection = new List<Ellipse>();
+        MainWindow myForm = Application.Current.Windows[0] as MainWindow;
+        static MatrixDraw instance = null;
+        public static MatrixDraw Create()
+        {
+            if (instance == null)
+                instance = new MatrixDraw();
+
+            return instance;
+        }
+
+        private MatrixDraw()
+        {
+
+        }
+
+        public void DrawMatrixFromConfig()
+        {
+            var _matrixHeight = Convert.ToInt16(myForm.matrixHeightTBox.Text.ToString());
+            var _matrixWidth = Convert.ToInt16(myForm.matrixWidthTBox.Text.ToString());
+            var _dotSize = Convert.ToInt16(myForm.dotSizeTBox.Text.ToString());
+            var _dotSpacing = Convert.ToInt16(myForm.dotSpacingTBox.Text.ToString());
+
+            double left = 0.0f;
+            double top = 0.0f;
+
+            myForm.myCanvas.Children.RemoveRange(0, myForm.myCanvas.Children.Count);
+
+            //StackPanel panel = new StackPanel();
+            for (var i = 0; i < _matrixHeight; i++)
+            {
+                for (var j = 0; j < _matrixWidth; j++)
+                {
+                    Ellipse ellipse = new Ellipse();
+                    SolidColorBrush solidBrush = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                    ellipse.Fill = solidBrush;
+                    ellipse.StrokeThickness = 0;
+                    ellipse.Width = _dotSize;
+                    ellipse.Height = _dotSize;
+                    left += _dotSpacing;
+                    ellipse.Margin = new Thickness(left, top, 0, 0);
+                    myForm.myCanvas.Children.Add(ellipse);
+
+                    ellipseCollection.Add(ellipse);
+                    //panel.Children.Add(elipse);
+                    //this.Content = panel;
+                }
+                top += _dotSpacing;
+                left = 0;
+            }
+        }
+
+        private void RedrawMatrix()
+        {
+            var _matrixHeight = Convert.ToInt16(myForm.matrixHeightTBox.Text.ToString());
+            var _matrixWidth = Convert.ToInt16(myForm.matrixWidthTBox.Text.ToString());
+
+            myForm.myCanvas.Children.RemoveRange(0, myForm.myCanvas.Children.Count);
+
+            for (var i = 0; i < _matrixHeight * _matrixWidth; i++)
+            {
+                myForm.myCanvas.Children.Add(ellipseCollection.ElementAt(i));
+            }
+        }
+
+        public void SetLed(short line, short column, Color color)
+        {
+            if (line > Convert.ToInt16(myForm.matrixHeightTBox.Text.ToString()))
+            {
+                MessageBox.Show("Line exceeds the maximum height");
+                return;
+            }
+            if (column > Convert.ToInt16(myForm.matrixWidthTBox.Text.ToString()))
+            {
+                MessageBox.Show("Column exceeds the maximum width");
+                return;
+            }
+
+            var ellipse = ellipseCollection.ElementAt((line - 1) * Convert.ToInt16(myForm.matrixWidthTBox.Text.ToString()) + (column - 1));
+            ellipse.Fill = new SolidColorBrush(color);
+            RedrawMatrix();
+
+        }
+
+        public void test()
+        {
+            
+            return;
+        }
     }
 }
